f988b209515792409fb631d445ad2317d162c102
KavitaBhatti <kavita1512b38@gmail.com>
2019-06-25 15:03:37 +0530

    E-Rickshaw

---
+++ app/src/debug/java/com/example/e_ricks/AutocompleteSupportFragment.java
@@ -0,0 +1,4 @@
+package com.example.e_ricks;
+
+class AutocompleteSupportFragment {
+}
---
+++ app/src/main/java/com/example/e_ricks/onAppKilled.java
@@ -0,0 +1,32 @@
+package com.example.e_ricks;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import com.firebase.geofire.GeoFire;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class onAppKilled extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("driversAvailable");
+        GeoFire geoFire = new GeoFire(ref);
+        geoFire.removeLocation(userId);
+    }
+}
